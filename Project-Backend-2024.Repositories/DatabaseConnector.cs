8ecb4a5b84ecfbebcda84291d0c17e8aaf3272c0
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-04-21 14:24:33 +0400

    docs:initializing repo with gitignore configured

---
+++ Project-Backend-2024.Repositories/DatabaseConnector.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_Backend_2024.Repositories
+{
+    public class DatabaseConnector : DbContext
+    {
+        public DatabaseConnector(DbContextOptions<DatabaseConnector> opts) : base(opts) { }
+    }
+}
