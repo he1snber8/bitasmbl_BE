ee1258ccd6c0df2e7023f1454afb6da5e37163e2
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-04-25 21:08:57 +0400

    created models and mapped it to DTOs using AutoMapper

--- Project-Backend-2024.DTO/Skill.cs
+++ Project-Backend-2024.DTO/Skill.cs
@@ -2,6 +2,6 @@
 
 public class Skill : IEntity
 {
-    public int Id { get; set; }
+    public int Id { get; }
     public string Name { get; set; } = null!;
 }
---
+++ Project-Backend-2024.Services/Profiles.cs
@@ -0,0 +1,16 @@
+﻿using Project_Backend_2024.Services.Models;
+using AutoMapper;
+using Project_Backend_2024.DTO;
+
+namespace Project_Backend_2024.Services;
+
+public class Profiles : Profile
+{
+    public Profiles()
+    {
+        CreateMap<User, UserModel>();
+        CreateMap<Project, ProjectModel>();
+        CreateMap<AppliedProject, AppliedProjectModel>();
+        CreateMap<UserSkills, UserSkillsModel>();
+    }
+}
