9ae94306ef836bc6b01f0c6a622ed52f1678781e
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-04-29 19:14:59 +0400

    implemented query services & refactored some

---
+++ Project-Backend-2024.Services/QueryServices/BaseQueryService.cs
@@ -0,0 +1,33 @@
+﻿
+using AutoMapper;
+using Project_Backend_2024.DTO;
+using Project_Backend_2024.Facade.Interfaces;
+using Project_Backend_2024.Services.Interfaces.Queries;
+using System.Linq.Expressions;
+
+namespace Project_Backend_2024.Services.QueryServices;
+
+public abstract class BaseQueryService<TEntity, TRepository> : IQueryModel<TEntity>
+    where TEntity : class, IEntity
+    where TRepository : IRepositoryBase<TEntity>
+{
+    protected readonly IMapper _mapper;
+    protected readonly IUnitOfWork _unitOfWork;
+    protected readonly TRepository _repository;
+
+    public BaseQueryService(IUnitOfWork unitOfWork, IMapper mapper, TRepository repository)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public TEntity GetById(int id) =>
+        _repository.Set(u => u.Id == id).SingleOrDefault() ?? throw new NotImplementedException();
+
+    public IQueryable<TEntity> Set(Expression<Func<TEntity, bool>> predicate) => _repository.Set(predicate);
+
+    public IEnumerable<TEntity> GetAll() => _repository.GetAll();
+
+    public IEnumerable<TEntity> SetMany(Expression<Func<TEntity, bool>> predicate) => _repository.SetMany(predicate);
+}
--- Project-Backend-2024/Program.cs
+++ Project-Backend-2024/Program.cs
@@ -1,51 +1,20 @@
-using Microsoft.EntityFrameworkCore;
-using Project_Backend_2024.Facade.Interfaces;
-using Project_Backend_2024.Repositories;
-using Project_Backend_2024.Services;
-using Project_Backend_2024.Services.CommandServices;
-using Project_Backend_2024.Services.Interfaces.Commands;
+using Project_Backend_2024;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.RegisterServices(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Configuration.AddJsonFile("appsettings.Development.json");
-
-builder.Services.AddDbContext<DatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLConnection")) //connection string
-        );
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserSkillsRepository, UserSkillsRepository>();
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-builder.Services.AddScoped<IAppliedProjectRepository, AppliedProjectRepository>();
-builder.Services.AddScoped<IUserSkillsRepository, UserSkillsRepository>();
-
-builder.Services.AddScoped<IUserCommandService, UserCommandService>();
-
-builder.Services.AddControllers();
-
-builder.Services.AddAutoMapper(typeof(Mappers).Assembly);
+builder.AddInternalFiles();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+Startup.UseSwagger(app);
 
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
+//app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
