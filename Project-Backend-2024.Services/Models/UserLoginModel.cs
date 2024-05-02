2f582c60518b42144ad81956289daed12401d700
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-02 18:27:47 +0400

    removed models from Services library

--- Project-Backend-2024.Services/Models/UserLoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Project_Backend_2024.DTO;
-
-namespace Project_Backend_2024.Services.Models;
-
-public class UserLoginModel : IAuthenticatable
-{
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
-}
