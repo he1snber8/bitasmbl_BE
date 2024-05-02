1579a15116d95456ba4c337d8d3787cf5e0c5d9d
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-02 18:28:08 +0400

    moved models folder to Facade library instead

---
+++ Project-Backend-2024.Facade/Models/AppliedProjectModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Backend_2024.Facade.Models;
+
+public class AppliedProjectModel : IEntityModel
+{   
+    public int Id { get; }
+    public string? CoverLetter { get; set; }
+    public string? ApplicationStatus { get; set; }
+    public int ProjectID { get; set; }
+    public int UserID { get; set; }
+}
---
+++ Project-Backend-2024.Facade/Models/ProjectModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Backend_2024.Facade.Models;
+
+public class ProjectModel : IEntityModel
+{
+    public int Id { get; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
+    public int PrincipalID { get; set; }
+}
