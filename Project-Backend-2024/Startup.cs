e93d09348cf75a04576613503b37b24a17de3d64
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-02 20:38:28 +0400

    organized startup configurations

--- Project-Backend-2024/Startup.cs
+++ Project-Backend-2024/Startup.cs
@@ -11,45 +11,45 @@ namespace Project_Backend_2024;
 
 internal static class Startup
 {
+
     internal static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
     {
+        // Bind JWT configuration
         var authConfiguration = new AuthConfiguration();
-
         configuration.Bind("JWT", authConfiguration);
         services.AddSingleton(authConfiguration);
 
+        // Add database context
         services.AddDbContext<DatabaseContext>(options =>
         {
             options.UseSqlServer(configuration.GetConnectionString("MSSQLConnection"));
         });
 
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-        /*REPOSITORIES HERE*/
+        // Register repositories
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ISkillRepository, SkillRepository>();
         services.AddScoped<IProjectRepository, ProjectRepository>();
         services.AddScoped<IAppliedProjectRepository, AppliedProjectRepository>();
         services.AddScoped<IUserSkillsRepository, UserSkillsRepository>();
-        /*COMMAND SERVICES HERE*/
+
+        // Register command services
         services.AddScoped<IUserCommandService, UserCommandService>();
         services.AddScoped<IProjectCommandService, ProjectCommandService>();
         services.AddScoped<IAppliedProjectCommandService, AppliedProjectCommandService>();
         services.AddScoped<ISkillCommandService, SkillCommandService>();
         services.AddScoped<IUserSkillsCommandService, UserSkillsCommandService>();
-        /*QUERY SERVICES HERE*/
 
+        // Register query services
 
-
-
-        /*OTHER SERVICES HERE*/
-        services.AddSingleton<IUserAccessToken,AccessTokenGenerator>();
+        // Register other services
+        services.AddSingleton<IUserAccessToken, AccessTokenGenerator>();
         services.AddControllers();
         services.AddAutoMapper(typeof(Mappers).Assembly);
 
         return services;
     }
 
-    internal static void UseSwagger(WebApplication app)
+    internal static void UseSwagger(this WebApplication app)
     {
         if (app.Environment.IsDevelopment())
             app.UseSwagger();
