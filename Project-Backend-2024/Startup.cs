51844a7e2dd2f0412b330b84b37523a166bbf608
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-03 21:39:33 +0400

    JWT configuration

--- Project-Backend-2024/Startup.cs
+++ Project-Backend-2024/Startup.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Project_Backend_2024.Facade.Interfaces;
 using Project_Backend_2024.Facade.Models;
 using Project_Backend_2024.Services;
 using Project_Backend_2024.Services.CommandServices;
 using Project_Backend_2024.Services.Interfaces.Commands;
+using Project_Backend_2024.Services.Interfaces.Queries;
+using Project_Backend_2024.Services.QueryServices;
 using Project_Backend_2024.Services.TokenGenerators;
 using Project_Backend_2024.Services.TokenGenerators.Interfaces;
+using System.Text;
 
 namespace Project_Backend_2024;
 
@@ -19,6 +25,21 @@ internal static class Startup
         configuration.Bind("JWT", authConfiguration);
         services.AddSingleton(authConfiguration);
 
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(bearer =>
+        {
+            bearer.TokenValidationParameters = new TokenValidationParameters()
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authConfiguration.Key)),
+                ValidIssuer = authConfiguration.Issuer,
+                ValidAudience = authConfiguration.Audience,
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ClockSkew = TimeSpan.Zero
+
+            };
+        });
+
         // Add database context
         services.AddDbContext<DatabaseContext>(options =>
         {
@@ -26,6 +47,7 @@ internal static class Startup
         });
 
         // Register repositories
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ISkillRepository, SkillRepository>();
         services.AddScoped<IProjectRepository, ProjectRepository>();
@@ -40,6 +62,7 @@ internal static class Startup
         services.AddScoped<IUserSkillsCommandService, UserSkillsCommandService>();
 
         // Register query services
+        services.AddScoped<IUserQueryService, UserQueryService>();
 
         // Register other services
         services.AddSingleton<IUserAccessToken, AccessTokenGenerator>();
@@ -49,10 +72,23 @@ internal static class Startup
         return services;
     }
 
-    internal static void UseSwagger(this WebApplication app)
+    internal static void AddSwagger(this WebApplication app)
     {
         if (app.Environment.IsDevelopment())
             app.UseSwagger();
             app.UseSwaggerUI();
+    }
+
+    internal static void AddSwaggerBearer(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(opts =>
+        {
+            opts.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { Scheme = "Bearer", In = ParameterLocation.Header, Type = SecuritySchemeType.Http, BearerFormat = "JWT" });
+
+            opts.AddSecurityRequirement(new OpenApiSecurityRequirement
+             {
+                { new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } } , new List<string>()}
+             });
+        });
     }
 }
