b751b0d3c8388607e205365c5e00e5c3b7a91b9f
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-09 18:53:21 +0400

    one-to-one relationship configured with user and token tables

---
+++ Project-Backend-2024.Repositories/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Project_Backend_2024.DTO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.HasIndex(t => t.Token)
+            .IsUnique();
+    }
+}
