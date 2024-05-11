35e2876064cae2e800d115f0c4839d8eb0d71dbf
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-11 14:14:22 +0400

    refactored user controller that handles jwt tokens for authorization. while it works, it could be improved

--- Project-Backend-2024/Controllers/CommandControllers/UserController.cs
+++ Project-Backend-2024/Controllers/CommandControllers/UserController.cs
@@ -5,28 +5,40 @@ using Project_Backend_2024.Services.Interfaces.Commands;
 using Project_Backend_2024.Facade.Models;
 using Project_Backend_2024.Facade.Responses;
 using Project_Backend_2024.Services.TokenGenerators;
+using Project_Backend_2024.Services.TokenValidators;
 
 namespace Project_Backend_2024.Controllers.CommandControllers;
 
-public class UserController : BaseCommandController<UserModel, IUserCommandService>
+[ApiController]
+[Route("commands/[controller]")]
+public class UserController : Controller
 {
+    private readonly IUserCommandService _userCommandService;
     private readonly AccessTokenGenerator _accessTokenGenerator;
     private readonly RefreshTokenGenerator _refreshTokenGenerator;
+    private readonly RefreshTokenValidator _refreshTokenValidator;
+    private readonly IRefreshTokenCommandService _refreshTokenRepository;
+    private readonly AuthConfiguration _authConfiguration;
 
-    public UserController(IUserCommandService command, AccessTokenGenerator accessTokenGenerator, RefreshTokenGenerator refreshTokenGenerator)
-        : base(command)
+    public UserController(IUserCommandService userCommandService, AccessTokenGenerator accessTokenGenerator,
+        RefreshTokenGenerator refreshTokenGenerator, RefreshTokenValidator refreshTokenValidator,
+        IRefreshTokenCommandService refreshTokenRepository, AuthConfiguration authConfiguration)
     {
+        _userCommandService = userCommandService;
         _accessTokenGenerator = accessTokenGenerator;
         _refreshTokenGenerator = refreshTokenGenerator;
+        _refreshTokenValidator = refreshTokenValidator;
+        _refreshTokenRepository = refreshTokenRepository;
+        _authConfiguration = authConfiguration;
     }
 
-
     [HttpPost("register")]
-    public override async Task<IActionResult> Insert([FromBody] UserModel model)
+    public async Task<IActionResult> Insert([FromBody] RegisterUserModel model)
     {
         try
         {
-            return await base.Insert(model);
+            await _userCommandService.Register(model);
+            return Ok("User registered successfully!");
         }
         catch (UsernameValidationException ex)
         {
@@ -43,25 +55,53 @@ public class UserController : BaseCommandController<UserModel, IUserCommandServi
     }
 
     [HttpPost("login")]
-    public IActionResult LogIn([FromBody] UserLoginModel loginModel)
+    public async Task<IActionResult> LogIn([FromBody] UserLoginModel loginModel)
     {
-        (bool isAuthenticated, User user) = _commandService.AutheticateLogin(loginModel);
+        (bool isAuthenticated, User user) = _userCommandService.AutheticateLogin(loginModel);
 
         if (isAuthenticated)
         {
-            string token = _accessTokenGenerator.GenerateToken(user);
-            string refreshToken = _refreshTokenGenerator.GenerateRefreshToken();
+            (string freshToken, string refreshToken) =
+            (_accessTokenGenerator.GenerateToken(user), 
+            _refreshTokenGenerator.GenerateRefreshToken());
 
-            return Ok(new AuthenticatedUserResponse
+            var token = await _refreshTokenRepository.GetByUserId(user.Id);
+
+            if (token == null)
             {
-                AccessToken = token,
-                RefreshToken = refreshToken
-            });
+                await _refreshTokenRepository.Insert(new RefreshTokenModel { UserID = user.Id, Token = refreshToken, 
+                    ExpirationDate = DateTime.Now.AddMinutes(_authConfiguration.RefreshTokenExpirationMinutes) });
+                return Ok(new AuthenticatedUserResponse(freshToken, refreshToken));
+            }
+
+            else if (token.ExpirationDate < DateTime.Now)
+            {
+                await _refreshTokenRepository.Update(token.Id, new RefreshTokenModel { UserID = user.Id, Token = refreshToken,
+                    ExpirationDate = DateTime.Now.AddMinutes(_authConfiguration.RefreshTokenExpirationMinutes) });
+                return Ok(new AuthenticatedUserResponse(freshToken, refreshToken));
+
+            }
+
+            return Ok(new AuthenticatedUserResponse(freshToken, token.Token));
+            
         }
 
         return Unauthorized();
     }
 
+    [HttpPost("refresh")]
+    public async Task<IActionResult> RefreshToken([FromBody] RefreshRequest refreshRequest)
+    {
+        if (!_refreshTokenValidator.Validate(refreshRequest.refreshToken)) return Unauthorized();
+
+        var modelByToken = await _refreshTokenRepository.GetByToken(refreshRequest.refreshToken);
+
+        var user = await _userCommandService.RetrieveUser(modelByToken.UserID);
+
+        string freshToken = _accessTokenGenerator.GenerateToken(user!);
+
+        return Ok(new AccessToken(freshToken))
+    }
 }
 
 
