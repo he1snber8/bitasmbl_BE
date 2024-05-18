9e67f35bd4a278cafd85f336c45d00136f91ea14
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-18 16:40:16 +0400

    implemented logging for user controller

--- Project-Backend-2024/Controllers/CommandControllers/UserController.cs
+++ Project-Backend-2024/Controllers/CommandControllers/UserController.cs
@@ -10,6 +10,8 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Project_Backend_2024.Services.Interfaces.Queries;
 using System.Reflection.Metadata.Ecma335;
+using Project_Backend_2024.Services.Authentication.Authorization;
+using Serilog;
 
 namespace Project_Backend_2024.Controllers.CommandControllers;
 
@@ -25,11 +27,12 @@ public class UserController : Controller
     private readonly IRefreshTokenQueryService _refreshTokenQueryService;
     private readonly IUserQueryService _userQueryService;
     private readonly AuthConfiguration _authConfiguration;
+    private readonly ILogger<UserController> _logger;
 
     public UserController(IUserCommandService userCommandService, AccessTokenGenerator accessTokenGenerator,
         RefreshTokenGenerator refreshTokenGenerator, RefreshTokenValidator refreshTokenValidator,
         IRefreshTokenCommandService refreshTokenCommand, AuthConfiguration authConfiguration,
-        IRefreshTokenQueryService refreshTokenQueryService, IUserQueryService userQueryService)
+        IRefreshTokenQueryService refreshTokenQueryService, IUserQueryService userQueryService, ILogger<UserController> logger)
     {
         _userCommandService = userCommandService;
         _accessTokenGenerator = accessTokenGenerator;
@@ -39,50 +42,77 @@ public class UserController : Controller
         _authConfiguration = authConfiguration;
         _refreshTokenQueryService = refreshTokenQueryService;
         _userQueryService = userQueryService;
+        _logger = logger;
     }
 
     [HttpPost("register")]
-    public async Task<IActionResult> Insert([FromBody] RegisterUserModel model)
+    public async Task<IActionResult> Register([FromBody] RegisterUserModel model)
     {
         try
         {
             await _userCommandService.Register(model);
+
+            _logger.LogInformation("{Date}: New user registered: {Username}",
+               DateTime.Now, model.Username);
+
             return Ok("User registered successfully!");
         }
         catch (UsernameValidationException ex)
         {
-            return BadRequest($"Username validation failed: {ex.Message}");
+            _logger.LogInformation("{Date}: username format error: {errorMessage}",
+               DateTime.Now, ex.Message);
+
+            return BadRequest($"Username format is invalid");
         }
         catch (PasswordValidationException ex)
         {
-            return BadRequest($"Password validation failed: {ex.Message}");
+            _logger.LogInformation("{Date}: password format error: {errorMessage}",
+               DateTime.Now, ex.Message);
+
+            return BadRequest($"Password format is invalid");
         }
         catch (EmailValidationException ex)
         {
-            return BadRequest($"Email validation failed: {ex.Message}");
+            _logger.LogInformation("{Date}: email format error: {errorMessage}",
+               DateTime.Now, ex.Message);
+
+            return BadRequest($"Email format is invalid");
         }
     }
 
     [HttpPost("login")]
+    //[HasPermission(Permission.WriteAccess,Permission.ReadAccess)]
     public async Task<IActionResult> LogIn([FromBody] UserLoginModel loginModel)
     {
-        (bool isAuthenticated, User user) = await _userCommandService.AutheticateLogin(loginModel);
-
-        if (isAuthenticated)
+        (bool isAuthenticated, UserModel user) = await _userCommandService.AutheticateLogin(loginModel);
+        try
         {
-            (string freshToken, string refreshToken) =
-            (_accessTokenGenerator.GenerateToken(user), 
-            _refreshTokenGenerator.GenerateRefreshToken());
+            if (isAuthenticated)
+            {
+                (string freshToken, string refreshToken) =
+                    (_accessTokenGenerator.GenerateToken(user),
+                    _refreshTokenGenerator.GenerateRefreshToken());
 
-            (var token,bool isAltered) = await _refreshTokenCommand.UpdateUserToken(user.Id,
-            DateTime.Now.AddMinutes(_authConfiguration.RefreshTokenExpirationMinutes),refreshToken);
+                (var token, bool isAltered) = await _refreshTokenCommand.UpdateUserToken(
+                    user.Id,
+                    DateTime.Now.AddMinutes(_authConfiguration.RefreshTokenExpirationMinutes),
+                    refreshToken);
 
-            if (isAltered) return Ok(new AuthenticatedUserResponse(freshToken, refreshToken));
+                if (isAltered) return Ok(new AuthenticatedUserResponse(freshToken, refreshToken));
 
-            else return Ok(new AuthenticatedUserResponse(freshToken, token.Token));
+                else return Ok(new AuthenticatedUserResponse(freshToken, token.Token));
+
+            }
+
+            return Unauthorized();
         }
+        catch (Exception ex)
+        {
+            _logger.LogInformation("{Date}: error while logging out: {errorMessage}",
+               DateTime.Now, ex.Message);
 
-        return Unauthorized();
+            return BadRequest();
+        }
     }
 
     [HttpPost("refresh")]
@@ -99,11 +129,19 @@ public class UserController : Controller
     [HttpDelete("logout")]
     public async Task<IActionResult> LogOut()
     {
-        var userId = User.FindFirstValue("Id");
+        try
+        {
+            var userId = User.FindFirstValue("Id");
 
-        int.TryParse(userId, out int tokenId);
+            int.TryParse(userId, out int tokenId);
 
-        await _refreshTokenCommand.InvalidateUserToken(tokenId);
+            await _refreshTokenCommand.InvalidateUserToken(tokenId);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation("{Date}: error while logging out: {errorMessage}",
+               DateTime.Now, ex.Message);
+        }
 
         return Ok("Logged out!");
     }
