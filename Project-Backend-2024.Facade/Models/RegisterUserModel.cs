8f880e9392c623c7e3abe03339731e6517ea98ab
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-05-27 16:57:03 +0400

    found the imposter! while mapping register model to a user, it defaulted to an id of 0 because model overwrote guid id of Identity

--- Project-Backend-2024.Facade/Models/RegisterUserModel.cs
+++ Project-Backend-2024.Facade/Models/RegisterUserModel.cs
@@ -2,9 +2,8 @@
 
 namespace Project_Backend_2024.Facade.Models;
 
-public class RegisterUserModel : IEntityModel, IAuthenticatable, IMailApplicable
+public class RegisterUserModel : IAuthenticatable, IMailApplicable
 {
-    public int Id { get; }
     public string Username { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
