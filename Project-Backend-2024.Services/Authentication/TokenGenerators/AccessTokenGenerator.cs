3893f98c50e6d6086bc4ba7ff69709e4b00c7542
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-06-01 21:09:44 +0400

    implemented cookie setup for tokens

--- Project-Backend-2024.Services/Authentication/TokenGenerators/AccessTokenGenerator.cs
+++ Project-Backend-2024.Services/Authentication/TokenGenerators/AccessTokenGenerator.cs
@@ -1,23 +1,45 @@
 ﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Project_Backend_2024.DTO;
 using Project_Backend_2024.Facade.Models;
 
 namespace Project_Backend_2024.Services.TokenGenerators;
 
-public class AccessTokenGenerator : TokenGenerator<UserConfiguration>
+public class AccessTokenGenerator : TokenGenerator
 {
-    public AccessTokenGenerator(UserConfiguration authConfiguration) : base(authConfiguration) { }
+    private readonly AuthConfiguration _authConfiguration;
 
-    public string GenerateToken(UserModel user)
+    public AccessTokenGenerator(AuthConfiguration authConfiguration)
     {
-        List<Claim> claims = new()
+        _authConfiguration = authConfiguration;
+    }
+
+    private string GenerateAccessToken(User user)
+         => GenerateToken(_authConfiguration.Key, _authConfiguration.Issuer,
+            _authConfiguration.Audience, _authConfiguration.AccessTokenExpirationMinutes,GenerateClaims(user));
+
+    public void SetAccessTokenInCookie(User user, HttpContext httpContext)
+    {
+        string newAccessToken = GenerateAccessToken(user);
+
+        httpContext.Response.Cookies.Append("accessToken", newAccessToken, new CookieOptions
         {
-            new Claim("Id", user.Id.ToString()),
-            new Claim(ClaimTypes.Email, user.Email),
-            new Claim (ClaimTypes.Name, user.Username),
+            Expires = DateTime.Now.AddMinutes(_authConfiguration.AccessTokenExpirationMinutes),
+            HttpOnly = true,
+            IsEssential = true,
+            Secure = true,
+            SameSite = SameSiteMode.None
+        });
+    }
+
+    private List<Claim> GenerateClaims(User user)
+    {
+        return new List<Claim>
+        {
+            new Claim("Id", user.Id),
+            new Claim(ClaimTypes.Email, user.Email!),
+            new Claim (ClaimTypes.Name, user.UserName!),
             new Claim(ClaimTypes.Role, "User")
         };
-
-        return  GenerateToken(_configuration.Key, _configuration.Issuer,
-            _configuration.Audience, _configuration.AccessTokenExpirationMinutes,claims);
     }
 }
---
+++ Project-Backend-2024.Services/Authentication/TokenGenerators/RefreshTokenGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Project_Backend_2024.DTO;
+using Project_Backend_2024.Facade.Models;
+using System.Security.Claims;
+
+namespace Project_Backend_2024.Services.TokenGenerators;
+
+public class RefreshTokenGenerator : TokenGenerator
+{
+    private readonly AuthConfiguration _authConfiguration;
+
+    public RefreshTokenGenerator(AuthConfiguration authConfiguration) { _authConfiguration = authConfiguration; }
+
+    private string GenerateRefreshToken()
+         => GenerateToken(_authConfiguration.RefreshKey,_authConfiguration.Issuer,
+            _authConfiguration.Audience,_authConfiguration.RefreshTokenExpirationMinutes);
+
+    public string SetRefreshTokenInCookie(HttpContext httpContext, string? storedRefreshToken = null)
+    {
+        string refreshToken = storedRefreshToken is null ? GenerateRefreshToken() : storedRefreshToken;
+
+        httpContext.Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
+        {
+            Expires = DateTime.Now.AddMinutes(_authConfiguration.RefreshTokenExpirationMinutes),
+            HttpOnly = true,
+            IsEssential = true,
+            Secure = true,
+            SameSite = SameSiteMode.None
+        });
+
+        return refreshToken;
+    }
+
+}
