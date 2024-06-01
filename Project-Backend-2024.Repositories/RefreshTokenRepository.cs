e719ecc47a986bf7482ccdbd3f75de1b4d61a177
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-06-01 21:15:35 +0400

    huge clearance of redundant code: Identity framework's built-in functionality made implemntation easier, removed some DIY code like password hashing and refresh token repo and much more

--- Project-Backend-2024.Repositories/RefreshTokenRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Project_Backend_2024.DTO;
-using Project_Backend_2024.Facade.Interfaces;
-using Project_Backend_2024.Repositories;
-
-public class RefreshTokenRepository : RepositoryBase<RefreshToken>, IRefreshTokenRepository
-{
-    public RefreshTokenRepository(DatabaseContext context) : base(context) { }
-}
