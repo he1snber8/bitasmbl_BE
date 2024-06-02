e162dfa91792539deae194a9d6d87712378a668b
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-06-02 22:13:48 +0400

    cleaned up startup file

--- Project-Backend-2024/Program.cs
+++ Project-Backend-2024/Program.cs
@@ -2,15 +2,8 @@
 using Project_Backend_2024;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddConfigurations(builder.Configuration);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.IncludeAppSettingsConfiguration(builder.Configuration);
+builder.AddConfigurations(builder.Configuration);
 
 var app = builder.Build();
-
 app.AddAppMiddleware();
 app.Run();
