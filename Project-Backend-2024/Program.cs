2563a45a3dc7e08fa3c499d05d84884527c750d9
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-06-02 16:53:07 +0400

    cleaner code after startup refactoring

--- Project-Backend-2024/Program.cs
+++ Project-Backend-2024/Program.cs
@@ -1,36 +1,16 @@
+
 using Project_Backend_2024;
-using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register services
-builder.Services.RegisterServices(builder.Configuration);
-builder.Services.AddSwaggerBearer();
+builder.Services.AddConfigurations(builder.Configuration);
 
-// Add controllers and API explorer
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Host.UseSerilog((context,config) =>
-{
-    config.ReadFrom.Configuration(context.Configuration);
-});
-
-// Clear default logging providers
-builder.Logging.ClearProviders();
-
-// Add Swagger
-builder.Services.AddSwaggerGen();
+builder.IncludeAppSettingsConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-// Middleware pipeline
-app.UseSerilogRequestLogging();
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.AddSwagger();
-app.MapControllers();
-
+app.AddAppMiddleware();
 app.Run();
