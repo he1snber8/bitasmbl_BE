c13b3da34e72a9b9736d04914deaaaf780eb0415
hei5nber8 <lukakhajavelidze@yahoo.com>
2024-06-11 13:56:38 +0400

    authorization service refactorings and user sign in logic added with http accessor

--- Project-Backend-2024.Services/Authentication/TokenGenerators/AccessTokenGenerator.cs
+++ Project-Backend-2024.Services/Authentication/TokenGenerators/AccessTokenGenerator.cs
@@ -1,5 +1,8 @@
 ﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Project_Backend_2024.DTO;
 using Project_Backend_2024.Facade.Models;
 
@@ -8,38 +11,41 @@ namespace Project_Backend_2024.Services.TokenGenerators;
 public class AccessTokenGenerator : TokenGenerator
 {
     private readonly AuthConfiguration _authConfiguration;
+    private readonly UserManager<User> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public AccessTokenGenerator(AuthConfiguration authConfiguration)
+    public AccessTokenGenerator(AuthConfiguration authConfiguration,UserManager<User> userManager, IHttpContextAccessor httpContextAccessor)
     {
         _authConfiguration = authConfiguration;
+        _userManager = userManager;
+         _httpContextAccessor = httpContextAccessor;
     }
 
-    private string GenerateAccessToken(User user)
-         => GenerateToken(_authConfiguration.Key, _authConfiguration.Issuer,
-            _authConfiguration.Audience, _authConfiguration.AccessTokenExpirationMinutes,GenerateClaims(user));
-
-    public void SetAccessTokenInCookie(User user, HttpContext httpContext)
+    public async Task GenerateAndSignIn(User user)
     {
-        string newAccessToken = GenerateAccessToken(user);
-
-        httpContext.Response.Cookies.Append("accessToken", newAccessToken, new CookieOptions
+        var roles = await _userManager.GetRolesAsync(user);
+        var claims = new List<Claim>
         {
-            Expires = DateTime.Now.AddMinutes(_authConfiguration.AccessTokenExpirationMinutes),
-            HttpOnly = true,
-            IsEssential = true,
-            Secure = true,
-            SameSite = SameSiteMode.None
-        });
-    }
-
-    private List<Claim> GenerateClaims(User user)
-    {
-        return new List<Claim>
-        {
-            new Claim("Id", user.Id),
-            new Claim(ClaimTypes.Email, user.Email!),
-            new Claim (ClaimTypes.Name, user.UserName!),
-            new Claim(ClaimTypes.Role, "User")
+        new Claim("Id", user.Id),
+        new Claim(ClaimTypes.Email, user.Email!),
+        new Claim (ClaimTypes.Name, user.UserName!)
         };
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var claimsIdentity = new ClaimsIdentity(claims, "MyAuthScheme");
+
+        await _httpContextAccessor.HttpContext
+            .SignInAsync("MyAuthScheme",
+            new ClaimsPrincipal(claimsIdentity),
+            new AuthenticationProperties());
+    }
+
+    public async Task SignOutAsync()
+    {
+        await _httpContextAccessor.HttpContext.SignOutAsync("MyAuthScheme");
     }
 }
--- Project-Backend-2024.Services/Authentication/TokenGenerators/RefreshTokenGenerator.cs
+++ Project-Backend-2024.Services/Authentication/TokenGenerators/RefreshTokenGenerator.cs
@@ -11,24 +11,7 @@ public class RefreshTokenGenerator : TokenGenerator
 
     public RefreshTokenGenerator(AuthConfiguration authConfiguration) { _authConfiguration = authConfiguration; }
 
-    private string GenerateRefreshToken()
+    public string GenerateRefreshToken()
          => GenerateToken(_authConfiguration.RefreshKey,_authConfiguration.Issuer,
             _authConfiguration.Audience,_authConfiguration.RefreshTokenExpirationMinutes);
-
-    public string SetRefreshTokenInCookie(HttpContext httpContext, string? storedRefreshToken = null)
-    {
-        string refreshToken = storedRefreshToken is null ? GenerateRefreshToken() : storedRefreshToken;
-
-        httpContext.Response.Cookies.Append("refreshToken", refreshToken, new CookieOptions
-        {
-            Expires = DateTime.Now.AddMinutes(_authConfiguration.RefreshTokenExpirationMinutes),
-            HttpOnly = true,
-            IsEssential = true,
-            Secure = true,
-            SameSite = SameSiteMode.None
-        });
-
-        return refreshToken;
-    }
-
 }
