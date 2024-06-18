945a60bd108b62a3dca94ade0e3a8bca7a643d18
lukakhaja <lukakhaja@yahoo.com>
2024-06-18 18:48:47 +0400

    Cookie and Cache setup

---
+++ Project-Backend-2024/StartupFolder/Startup.Authentication.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Project_Backend_2024.Facade.Models;
+
+namespace Project_Backend_2024.StartupFolder;
+internal static partial class Startup
+{
+    internal static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var cacheAuthConfiguration = new CacheConfiguration();
+        configuration.Bind("Cache", cacheAuthConfiguration);
+        services.AddSingleton(cacheAuthConfiguration);
+        
+        var cookieConfiguration = new CookieConfiguration();
+        configuration.Bind("Cookie", cookieConfiguration);
+        services.AddSingleton(cookieConfiguration);
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+              options.SlidingExpiration = true; 
+              options.ExpireTimeSpan = TimeSpan.FromSeconds(cookieConfiguration.ExpirationSeconds);
+            });
+        
+        services.AddControllers();
+        services.AddHttpContextAccessor();
+        services.AddMemoryCache();
+    }
+}
+
+
