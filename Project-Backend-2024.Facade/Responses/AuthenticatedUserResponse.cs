67b6707c7c32a1ce25adce614bcb191608daea57
lukakhaja <lukakhaja@yahoo.com>
2024-06-24 11:17:37 +0400

    some more cleanup

--- Project-Backend-2024.Facade/Responses/AuthenticatedUserResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Project_Backend_2024.Facade.Responses;
-
-public record AuthenticatedUserResponse(string AccessToken, string RefreshToken);
-
-public record AuthenticatedAdminResponse(string AccessToken, string RefreshToken);
