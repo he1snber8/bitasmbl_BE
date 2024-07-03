e984f8157b7c1810b0b49eeeac36b80e74462cb9
lukakhaja <lukakhaja@yahoo.com>
2024-07-03 13:26:49 +0400

    removed some interfaces that were of no use

--- Project-Backend-2024.DTO/Interfaces/Entities.cs
+++ Project-Backend-2024.DTO/Interfaces/Entities.cs
@@ -1,15 +1,5 @@
 ﻿namespace Project_Backend_2024.DTO.Interfaces;
 
-public interface IEntity<out T> { T Id { get; } }
-
+public interface IBaseEntity { }
+public interface IEntity<out T> : IBaseEntity { T Id { get; } }
 public interface IEntity :  IEntity<int> { }
-
-public interface IDeletable { bool IsDeleted { get; set; } }
-
-public interface IAuthenticatable /* interface for authenticatable entities like User */
-{
-    string Username { get; set; }
-    string Password { get; set; }
-}
-
-public interface IMailApplicable { string Email { get; set; } }
