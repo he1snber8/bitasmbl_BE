8c4576366cd77092d55ecba43843efa8a6ef4e3c
lukakhaja <lukakhaja@yahoo.com>
2024-07-06 18:01:22 +0400

    some removals

--- Project-Backend-2024.Services/QueryServices/Projects/Get/GetMyProjectsQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using Project_Backend_2024.Facade.BasicGetModels;
-using Project_Backend_2024.Facade.BasicInsertModels;
-
-namespace Project_Backend_2024.Services.QueryServices.Projects.Get;
-
-public record GetMyProjectsQuery() : IRequest<List<ProjectBasicGetModel?>?>;
