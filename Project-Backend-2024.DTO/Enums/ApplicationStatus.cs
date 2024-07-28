d8310e7cc6086008dc546d63b2952af07f8579c3
lukakhaja <lukakhaja@yahoo.com>
2024-07-28 19:21:05 +0400

    app status config retouch, this way, applied string is parsed into enum and respectively added in db, avoiding hard-coding strings

---
+++ Project-Backend-2024.DTO/Enums/ApplicationStatus.cs
@@ -0,0 +1,9 @@
+namespace Project_Backend_2024.DTO.Enums;
+
+public enum ApplicationStatus
+{
+    Pending,
+    UnderReview,
+    Approved,
+    Rejected
+}
---
+++ Project-Backend-2024.Repositories/Configurations/ProjectApplicationConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_Backend_2024.DTO;
+using Project_Backend_2024.DTO.Enums;
+
+namespace Project_Backend_2024.Repositories.Configurations;
+
+public class ProjectApplicationConfiguration : IEntityTypeConfiguration<ProjectApplication>
+{
+    public void Configure(EntityTypeBuilder<ProjectApplication> builder)
+    {
+        builder.HasKey(ap => ap.Id);
+
+        builder.HasIndex(p => new { p.ProjectId, p.ApplicantId}).IsUnique();
+
+        builder.Property(ap => ap.ApplicationStatus)
+            .HasConversion(
+                v => v.ToString(),
+                v => (ApplicationStatus)Enum.Parse(typeof(ApplicationStatus), v!))
+            .HasColumnType("varchar")
+            .HasMaxLength(50)
+            .HasDefaultValueSql("'Pending'"); 
+
+        builder.Property(ap => ap.DateApplied)
+            .HasColumnType("datetime")
+            .HasDefaultValueSql("GETDATE()");
+
+        builder.Property(ap => ap.ApplicantId)
+            .HasColumnType("nvarchar")
+            .HasMaxLength(450);
+        
+        builder.Property(ap => ap.PrincipalId)
+            .HasColumnType("nvarchar")
+            .HasMaxLength(450);
+
+        builder.Property(ap => ap.CoverLetter)
+            .HasColumnType("nvarchar")
+            .HasMaxLength(450);
+
+        builder.Property(ap => ap.Applicant)
+            .HasColumnType("varchar")
+            .HasMaxLength(30)
+            .IsRequired(false);
+
+        builder.Property(ap => ap.Applicant)
+            .HasColumnType("varchar")
+            .HasMaxLength(30)
+            .IsRequired(false);
+
+        builder.HasOne(ap => ap.User)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction)
+            .HasForeignKey(ap => ap.ApplicantId);
+
+        builder.HasOne(ap => ap.Project)
+            .WithMany(a => a.Applications)
+            .HasForeignKey(ap => ap.ProjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
