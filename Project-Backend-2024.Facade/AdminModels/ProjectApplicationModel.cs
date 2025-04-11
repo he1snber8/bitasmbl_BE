c0b6bed3fcfd2f1e2c9956f2caba8d6435cdc3e5
lukakhaja <lukakhaja@yahoo.com>
2025-04-11 12:36:16 +0400

    Add: admin models

---
+++ Project-Backend-2024.Facade/AdminModels/ProjectApplicationModel.cs
@@ -0,0 +1,20 @@
+﻿using Project_Backend_2024.Facade.Interfaces;
+
+namespace Project_Backend_2024.Facade.AdminModels;
+
+public class ProjectApplicationModel : IEntityModel
+{   
+    public int Id { get; set; }
+
+    public string PrincipalId { get; set; } = null!;
+    
+    public string? CoverLetter { get; set; }
+    
+    public string? ApplicationStatus { get; set; }
+    
+    public int ProjectId { get; set; }
+
+    public string ApplicantId { get; set; } = null!;
+    
+    public string Applicant { get; set; } = null!;
+}
---
+++ Project-Backend-2024.Facade/AdminModels/ProjectModel.cs
@@ -0,0 +1,14 @@
+﻿using Project_Backend_2024.Facade.Interfaces;
+
+namespace Project_Backend_2024.Facade.AdminModels;
+
+public class ProjectModel : IEntityModel
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
+    public DateTime? DateCreated { get; set; }
+    public string PrincipalId { get; set; } = null!;
+    public ICollection<ProjectApplicationModel>? Applications { get; set; } 
+}
