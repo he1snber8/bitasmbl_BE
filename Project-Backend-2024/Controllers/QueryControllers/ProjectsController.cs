2474e9d4e932f20f782ee40d36998b0df5ad19df
lukakhaja <lukakhaja@yahoo.com>
2025-04-12 12:29:36 +0400

    Refactor: query controllers

--- Project-Backend-2024/Controllers/QueryControllers/ProjectsController.cs
+++ Project-Backend-2024/Controllers/QueryControllers/ProjectsController.cs
@@ -2,70 +2,115 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Project_Backend_2024.Facade.Exceptions;
+using Project_Backend_2024.Facade.FetchModels;
 using Project_Backend_2024.Services.QueryServices.ProjectApplications.Get;
+using Project_Backend_2024.Services.QueryServices.Projects.Get;
 using Project_Backend_2024.Services.QueryServices.Projects.List;
 
 namespace Project_Backend_2024.Controllers.QueryControllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProjectsController(ISender sender) : Controller
+public class ProjectsController(ISender sender, ILogger<ProjectsController> logger) : Controller
 {
     [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
     [HttpGet("profile")]
-    public async Task<IActionResult> GetUserProjects()
+    public async Task<IActionResult> GetUserProjects([FromQuery] string? projectName)
     {
         try
         {
-            var projectModels = await sender.Send(new ListMyProjectsQuery());
+            logger.LogInformation("Attempting to retrieve user's projects.");
 
-            return projectModels is null || 
-                   projectModels.Count == 0 
-                ? Ok("You have no projects, create one!") 
-                : Ok(projectModels);
+            var projectModels = projectName is not null
+                ? await sender.Send(new ListMyProjectsQuery(projectName))
+                : await sender.Send(new ListMyProjectsQuery());
+
+            if (projectModels is null || projectModels.Count == 0)
+            {
+                logger.LogInformation("No projects found for the user.");
+                return Ok("No projects found. Start by creating your first project!");
+            }
+
+            logger.LogInformation("Successfully retrieved user's projects.");
+            return Ok(projectModels);
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return BadRequest("Something went wrong");
+            logger.LogError(ex, "Failed to retrieve user's projects.");
+            return BadRequest("An error occurred while fetching your projects. Please try again later.");
         }
     }
-    
-    // [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
+
+    [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
     [AllowAnonymous]
     [HttpGet]
-    public  async Task<IActionResult> GetAllProjects()
+    public async Task<IActionResult> ListAllProjects()
     {
         try
         {
-            var projectModels = await sender.Send(new ListAllProjectsQuery());
-    
-            return projectModels is null ? Ok("Projects not found") : Ok(projectModels);
+            logger.LogInformation("Fetching all available projects.");
+
+            var projects = await sender.Send(new ListAllProjectsQuery());
+
+            if (projects?.Count == 0)
+            {
+                logger.LogInformation("No projects available in the system.");
+                return Ok("Currently, there are no projects available. Check back later!");
+            }
+
+            logger.LogInformation("Successfully retrieved all projects.");
+            return Ok(projects);
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return BadRequest("Something went wrong");
+            logger.LogError(ex, "Failed to retrieve all projects.");
+            return Unauthorized("You are not authorized to view these projects.");
         }
     }
-    
+
     [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
-    [HttpGet("applications")]
-    public async Task<IActionResult> GetMyProjectApplications([FromQuery]string? project=null)
+    [HttpGet("applications/project/{id:int}")]
+    public async Task<IActionResult> GetMyProjectApplications(int? id)
     {
         try
         {
-            var applicationModels = project is not null ?
-                await sender.Send(new GetMyProjectApplicationsQuery(project)) :
-                await sender.Send(new GetMyProjectApplicationsQuery());
-
-            return applicationModels.Count == 0 ? Ok("You have no applications yet") : Ok(applicationModels);
+            var applicationModels = id is not null
+                ? await sender.Send(new GetMyProjectApplicationsQuery(id))
+                : await sender.Send(new GetMyProjectApplicationsQuery(0));
+    
+            return applicationModels.Count == 0
+                ? Ok("No applications found for your projects.")
+                : Ok(applicationModels);
         }
         catch (ProjectNotFoundException ex)
         {
             return BadRequest(ex.Message);
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return BadRequest("Something went wrong");
+            logger.LogError(ex, "An error occurred while retrieving project applications.");
+            return BadRequest("Unable to fetch project applications. Please try again later.");
+        }
+    }
+
+    [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetMyProjectApplication(int id)
+    {
+        try
+        {
+            var projectModel = await sender.Send(new GetProjectByIdQuery(id));
+            
+            return Ok(projectModel);
+        }
+        catch (ProjectNotFoundException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while retrieving project applications.");
+            return BadRequest("Unable to fetch project applications. Please try again later.");
         }
     }
     
@@ -75,18 +120,71 @@ public class ProjectsController(ISender sender) : Controller
     {
         try
         {
+            logger.LogInformation("Fetching projects the user has applied to.");
+
             var appliedProjects = await sender.Send(new GetMyAppliedProjectsQuery());
 
-            return appliedProjects.Count == 0 ? Ok("You have not applied to any projects yet") : Ok(appliedProjects);
+            if (appliedProjects.Count == 0)
+            {
+                logger.LogInformation("No applied projects found for the user.");
+                return Ok("You haven't applied to any projects yet.");
+            }
+
+            logger.LogInformation("Successfully retrieved applied projects.");
+            return Ok(appliedProjects);
         }
         catch (ProjectNotFoundException ex)
         {
+            logger.LogWarning(ex, "No applied projects found for the user.");
             return BadRequest(ex.Message);
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return BadRequest("Something went wrong");
+            logger.LogError(ex, "An error occurred while retrieving applied projects.");
+            return BadRequest("Unable to fetch your applied projects. Please try again later.");
         }
     }
 
+    [HttpGet("categories")]
+    public async Task<IActionResult> GetProjectCategories()
+    {
+        try
+        {
+            var categories = await sender.Send(new GetCategoriesModel());
+            return Ok(categories);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("requirements")]
+    public async Task<IActionResult> GetProjectRequirements()
+    {
+        try
+        {
+            var requirements = await sender.Send(new GetRequirementsModel());
+            return Ok(requirements);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet("requirement/tests/")]
+    public async Task<IActionResult> GetProjectRequirementTests([FromQuery] string requirementNames)
+    {
+        try
+        {
+            var requirementTests = await sender.Send(new GetRequirementTest(requirementNames));
+            
+            return Ok(requirementTests);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
 }
--- Project-Backend-2024/Controllers/QueryControllers/UsersController.cs
+++ Project-Backend-2024/Controllers/QueryControllers/UsersController.cs
@@ -1,66 +1,233 @@
-﻿using MediatR;
+﻿using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Project_Backend_2024.DTO;
+using Newtonsoft.Json;
+using Project_Backend_2024.Controllers.CommandControllers;
+using Project_Backend_2024.DTO.Github;
+using Project_Backend_2024.Facade.FetchModels;
 using Project_Backend_2024.Facade.GetModels;
 using Project_Backend_2024.Services.QueryServices.Users.Get;
 using Project_Backend_2024.Services.QueryServices.Users.List;
+using User = Project_Backend_2024.DTO.User;
 
 
 namespace Project_Backend_2024.Controllers.QueryControllers;
 
 [ApiController]
 [Route("[controller]")]
-public class UsersController(UserManager<User> userManager,ISender sender) : Controller
+public class UsersController(UserManager<User> userManager, ISender sender, ILogger<UsersController> logger)
+    : Controller
 {
-    [Authorize(AuthenticationSchemes = "Cookies", Policy="AdminOrUser")]
-    [HttpGet("")]
+    // [Authorize(AuthenticationSchemes = "Cookies", Policy="AdminOrUser")]
+    [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<List<GetUserModel>>> ListAllUsers()
     {
         try
-        { 
-            return Ok(await sender.Send(new ListAllUsersQuery()));
-        }
-        catch (Exception)
         {
-            return Unauthorized("You are not authorized");
+            logger.LogInformation("Fetching all users.");
+
+            var users = await sender.Send(new ListAllUsersQuery());
+
+            if (users is null || users.Count == 0)
+            {
+                logger.LogInformation("No users found.");
+                return Ok("No users are currently registered.");
+            }
+
+            logger.LogInformation("Successfully retrieved all users.");
+            return Ok(users);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while fetching users.");
+            return Unauthorized("You are not authorized to access user data.");
         }
     }
 
     [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
-    [HttpGet("{email}")]
-    public async Task<IActionResult> GetByEmail([FromQuery] string email)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByUserById(string id)
     {
         try
         {
-            var userModel = await sender.Send(new GetUserByEmailQuery(email));
-            
+            var userModel = await sender.Send(new GetUserByIdQuery(id));
+
+            if (userModel is null)
+                return NotFound($"Could not find user");
+
             return Ok(userModel);
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            return Unauthorized("you are not authorized, fuck off");
+            logger.LogError(ex.Message, "An error occurred while fetching user");
+            return Unauthorized("Error");
         }
+    }
+
+    [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
+    [HttpGet("profile")]
+    public async Task<IActionResult> GetProfile()
+    {
+        try
+        {
+            logger.LogInformation("Fetching profile for the authenticated user.");
+            var userModel = await sender.Send(new GetUserProfileQuery());
+
+            logger.LogInformation("Successfully retrieved the user's profile.");
+            return Ok(userModel);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while fetching the user's profile.");
+            return Unauthorized("You are not authorized to access your profile.");
+        }
+    }
+    
+    [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
+    [HttpGet("github/repos")]
+    public async Task<IActionResult> GetGithubUserRepos([FromQuery] string accessToken, [FromQuery] string username)
+    {
+        using var httpClient = new HttpClient();
+
+        // var accessToken = await sender.Send(new GetGithubAccessToken(code));
+
+        if (string.IsNullOrEmpty(accessToken))
+            return BadRequest("Access token missing in response.");
+
+        var reposUrl = $"https://api.github.com/users/{username}/repos";
+
+        var request = new HttpRequestMessage(HttpMethod.Get, reposUrl);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        request.Headers.UserAgent.ParseAdd("Bitasmbl");
+
+        var repoResponse = await httpClient.SendAsync(request);
+
+        var repoResponseBody = await repoResponse.Content.ReadAsStringAsync();
+
+        var deserializedRepos = JsonConvert.DeserializeObject<List<GithubRepo>>(repoResponseBody)
+                                ?? throw new SerializationException();
+
+        return Ok(deserializedRepos);
+    }
+
+
+    [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
+    [HttpGet("github/repo/commits")]
+    public async Task<IActionResult> GetGithubUserRepoCommits([FromQuery] string accessToken,
+        [FromQuery] string username, [FromQuery] string repo, [FromQuery] string branch = "main")
+    {
+        using var httpClient = new HttpClient();
+
+        // var accessToken = await sender.Send(new GetGithubAccessToken(code));
+
+        if (string.IsNullOrEmpty(accessToken))
+            return BadRequest("Access token missing in response.");
+
+        var commitsUrl = $"https://api.github.com/repos/{username}/{repo}/commits?sha={branch}";
+
+        var request = new HttpRequestMessage(HttpMethod.Get, commitsUrl);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        request.Headers.UserAgent.ParseAdd("Bitasmbl");
+
+        var commitsResponse = await httpClient.SendAsync(request);
+
+        var repoResponseBody = await commitsResponse.Content.ReadAsStringAsync();
+
+        if (string.IsNullOrWhiteSpace(repoResponseBody))
+            return Ok(new { message = "empty commits" });
+
+        if (repoResponseBody.TrimStart().StartsWith("{")) // If response is a JSON object
+        {
+            var deserializedCommit = JsonConvert.DeserializeObject<GithubCommit>(repoResponseBody)
+                                      ?? throw new SerializationException();
+            
+            return Ok(deserializedCommit);
+        }
+        var deserializedCommits = JsonConvert.DeserializeObject<List<GithubCommit>>(repoResponseBody)
+                                  ?? throw new SerializationException();
+
+        return Ok(deserializedCommits);
+    }
+    
+    [Authorize(AuthenticationSchemes = "Cookies", Policy = "AdminOrUser")]
+    [HttpGet("github/repo/branches")]
+    public async Task<IActionResult> GetGithubUserRepoBranches([FromQuery] string accessToken,
+        [FromQuery] string username, [FromQuery] string repo)
+    {
+        using var httpClient = new HttpClient();
+
+        // var accessToken = await sender.Send(new GetGithubAccessToken(code));
+
+        if (string.IsNullOrEmpty(accessToken))
+            return BadRequest("Access token missing in response.");
+
+        var commitsUrl = $"https://api.github.com/repos/{username}/{repo}/branches";
+
+        var request = new HttpRequestMessage(HttpMethod.Get, commitsUrl);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        request.Headers.UserAgent.ParseAdd("Bitasmbl");
+
+        var commitsResponse = await httpClient.SendAsync(request);
+
+        var repoResponseBody = await commitsResponse.Content.ReadAsStringAsync();
+
+        if (string.IsNullOrWhiteSpace(repoResponseBody))
+            return Ok(new { message = "empty commits" });
+
+        if (repoResponseBody.TrimStart().StartsWith("{")) // If response is a JSON object
+        {
+            var deserializedCommit = JsonConvert.DeserializeObject<GithubCommit>(repoResponseBody)
+                                     ?? throw new SerializationException();
+            
+            return Ok(deserializedCommit);
+        }
+        var deserializedCommits = JsonConvert.DeserializeObject<List<GithubCommit>>(repoResponseBody)
+                                  ?? throw new SerializationException();
+
+        return Ok(deserializedCommits);
     }
 
     [HttpGet("confirm")]
-    public async Task<IActionResult> ConfirmEmail([FromQuery]string? userId,[FromQuery]string? token)
+    public async Task<IActionResult> ConfirmEmail([FromQuery] string? userId, [FromQuery] string? token)
     {
-        if (userId is null || token is null)
+        if (string.IsNullOrWhiteSpace(userId) || string.IsNullOrWhiteSpace(token))
+        {
+            logger.LogWarning("Invalid email confirmation request: Missing userId or token.");
             return BadRequest("Invalid email confirmation request.");
+        }
 
-        var user = await userManager.FindByIdAsync(userId);
-            
-        if (user is null)
-            return BadRequest("User not found.");
-        
-        var result = await userManager.ConfirmEmailAsync(user, token);
-        
-        if (result.Succeeded)
-            return Ok("Email confirmed successfully.");
-        
-        return BadRequest("Email confirmation failed.");
+        try
+        {
+            logger.LogInformation("Attempting to confirm email for userId: {UserId}", userId);
+
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user is null)
+            {
+                logger.LogWarning("User not found for userId: {UserId}", userId);
+                return BadRequest("User not found.");
+            }
+
+            var result = await userManager.ConfirmEmailAsync(user, token);
+
+            if (result.Succeeded)
+            {
+                logger.LogInformation("Email successfully confirmed for userId: {UserId}", userId);
+                return Ok("Email confirmed successfully.");
+            }
+
+            logger.LogWarning("Email confirmation failed for userId: {UserId}", userId);
+            return BadRequest("Email confirmation failed. Please ensure the link is correct and try again.");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred during email confirmation for userId: {UserId}", userId);
+            return BadRequest("An error occurred while confirming the email. Please try again later.");
+        }
     }
-
 }
