0303b43deceea270106ea962b966bb4922e7dc58
lukakhaja <lukakhaja@yahoo.com>
2025-04-12 12:25:23 +0400

    Delete: redundant dll and classes

--- Project-Backend-2024.InterfaceGateway/IAuthenticatable.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Project_Backend_2024.Facade.Interfaces;
-
-public interface IAuthenticatable
-{
-    string Username { get; set; }
-    string Password { get; set; }
-}
--- Project-Backend-2024.InterfaceGateway/IDeletable.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Project_Backend_2024.Facade.Interfaces;
-
-public interface IDeletable { bool IsDeleted { get; set; } }
