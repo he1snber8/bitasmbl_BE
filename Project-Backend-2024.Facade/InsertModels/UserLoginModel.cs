c5fab04ff9ff021c2fa9a5051060344ffea82bb6
lukakhaja <lukakhaja@yahoo.com>
2025-04-12 12:26:37 +0400

    Remove: deleted files

--- Project-Backend-2024.Facade/InsertModels/UserLoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Project_Backend_2024.DTO.Interfaces;
-
-namespace Project_Backend_2024.Facade.InsertModels;
-
-public class UserLoginModel
-{
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
-}
--- Project-Backend-2024.Repositories/SkillRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Project_Backend_2024.DTO;
-using Project_Backend_2024.Facade.Interfaces;
-using Project_Backend_2024.Repositories;
-
-public class SkillRepository : RepositoryBase<Skill>, ISkillRepository
-{
-    public SkillRepository(DatabaseContext context) : base(context) { }
-}
